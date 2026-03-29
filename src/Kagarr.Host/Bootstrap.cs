bf0bce09ca5abde5240d1e52338500362c794b7d
ronald50929 <ronaldrodriguez50928@gmail.com>
2026-03-29 12:10:35 -0400

    fix(build): update OpenAPI namespace for Swashbuckle 10

--- src/Kagarr.Host/Bootstrap.cs
+++ src/Kagarr.Host/Bootstrap.cs
@@ -24,7 +24,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using NLog.Extensions.Logging;
 
 namespace Kagarr.Host
